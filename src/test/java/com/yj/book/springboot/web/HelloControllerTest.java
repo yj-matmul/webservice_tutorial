4a4d37c1f12f2973ccfa11bd4ce4e3e381e3834a
yj-matmul <yyj3053@gmail.com>
2021-04-24 23:52:20 +0900

    [20210424] update HelloControllerTest

--- src/test/java/com/yj/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/yj/book/springboot/web/HelloControllerTest.java
@@ -33,7 +33,6 @@ public class HelloControllerTest {
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
-
         mvc.perform(get("/hello/dto")
                         .param("name", name)
                         .param("amount", String.valueOf(amount)))
